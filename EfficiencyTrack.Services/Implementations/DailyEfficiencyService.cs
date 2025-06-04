ddd859ca51ecffdce7c06beedf0de33e45f47c04
VelizarABB <velizar.stankov@bg.abb.com>
2025-06-04 13:53:01 +0300

    add Daily efficiency service

---
+++ EfficiencyTrack.Services/Implementations/DailyEfficiencyService.cs
@@ -0,0 +1,55 @@
+﻿using EfficiencyTrack.Data.Data;
+using EfficiencyTrack.Data.Models;
+using EfficiencyTrack.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfficiencyTrack.Services.Implementations
+{
+    public class DailyEfficiencyService : CrudService<DailyEfficiency>, IDailyEfficiencyService
+    {
+        private readonly EfficiencyTrackDbContext _context;
+
+        public DailyEfficiencyService(
+            EfficiencyTrackDbContext context,
+            IHttpContextAccessor httpContextAccessor)
+            : base(context, httpContextAccessor)
+        {
+            _context = context;
+        }
+
+        public async Task<decimal> CalculateDailyEfficiencyAsync(Guid employeeId, DateTime date)
+        {
+            var entriesOfDay = await _context.Entries
+                .AsNoTracking()
+                .Where(e => e.EmployeeId == employeeId
+                         && e.Date.Date == date.Date
+                         && !e.IsDeleted)
+                .ToListAsync();
+
+            if (!entriesOfDay.Any())
+            {
+                return 0m;
+            }
+
+            decimal totalNeededMinutes = entriesOfDay
+                .Sum(e => e.RequiredMinutes);
+
+            var shiftId = entriesOfDay.First().ShiftId;
+
+            var shiftDuration = await _context.Shifts
+                .Where(s => s.Id == shiftId)
+                .Select(s => s.DurationMinutes)
+                .FirstOrDefaultAsync();
+
+            if (shiftDuration == 0)
+            {
+                return 0m;
+            }
+
+            decimal efficiencyPercent = (totalNeededMinutes / shiftDuration) * 100;
+
+            return efficiencyPercent;
+        }
+    }
+}
---
+++ EfficiencyTrack.Services/Interfaces/IDailyEfficiencyService.cs
@@ -0,0 +1,9 @@
+﻿using EfficiencyTrack.Data.Models;
+
+namespace EfficiencyTrack.Services.Interfaces
+{
+    public interface IDailyEfficiencyService : ICrudService<DailyEfficiency>
+    {
+        Task<decimal> CalculateDailyEfficiencyAsync(Guid employeeId, DateTime date);
+    }
+}
