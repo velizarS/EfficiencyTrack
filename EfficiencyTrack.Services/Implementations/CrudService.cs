cb57ee4038a559ac736fe7d6652e16254c9288c7
VelizarABB <velizar.stankov@bg.abb.com>
2025-06-04 10:59:28 +0300

    Common service with CRUD operation

---
+++ EfficiencyTrack.Services/Implementations/CrudService.cs
@@ -0,0 +1,57 @@
+﻿using EfficiencyTrack.Data.Data;
+using EfficiencyTrack.Data.Models;
+using EfficiencyTrack.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+public class CrudService<T> : ICrudService<T> where T : BaseEntity
+{
+    private readonly EfficiencyTrackDbContext _context;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CrudService(EfficiencyTrackDbContext context, IHttpContextAccessor httpContextAccessor)
+    {
+        _context = context;
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+        => await _context.Set<T>()
+                         .AsNoTracking()
+                         .Where(e => !e.IsDeleted)
+                         .ToListAsync();
+
+    public async Task<T?> GetByIdAsync(Guid id)
+        => await _context.Set<T>()
+                         .AsNoTracking()
+                         .FirstOrDefaultAsync(e => e.Id == id && !e.IsDeleted);
+
+    public async Task AddAsync(T entity)
+    {
+        entity.CreatedOn = DateTime.UtcNow;
+        entity.CreatedBy = _httpContextAccessor.HttpContext?.User?.Identity?.Name ?? "System";
+
+        _context.Set<T>().Add(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        entity.ModifiedOn = DateTime.UtcNow;
+        entity.ModifiedBy = _httpContextAccessor.HttpContext?.User?.Identity?.Name ?? "System";
+
+        _context.Set<T>().Update(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var entity = await _context.Set<T>().FindAsync(id);
+        if (entity != null)
+        {
+            entity.IsDeleted = true;
+            entity.DeletedBy = _httpContextAccessor.HttpContext?.User?.Identity?.Name ?? "System";
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ EfficiencyTrack.Services/Interfaces/ICrudService.cs
@@ -0,0 +1,14 @@
+﻿using EfficiencyTrack.Data.Models;
+
+namespace EfficiencyTrack.Services.Interfaces
+{
+    public interface ICrudService<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(Guid id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(Guid id);
+    }
+
+}
