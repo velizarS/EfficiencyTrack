3756e8e24521488ee1d80a3831d20d3fb03d76c0
VelizarABB <velizar.stankov@bg.abb.com>
2025-06-28 17:02:35 +0300

    migration for employe update

---
+++ EfficiencyTrack.Data/Data/Migrations/20250628140150_EmployeModelUpdate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfficiencyTrack.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmployeModelUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Employees");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Employees",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                comment: "Indicates if the employee is currently active.");
+        }
+    }
+}
