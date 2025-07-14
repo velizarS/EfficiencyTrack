239b5b6ee905ccf3fe3677b2fa681740fdcc3d6f
VelizarABB <velizar.stankov@bg.abb.com>
2025-07-14 16:26:48 +0300

    Add common project for common stuff

---
+++ Common/EfficiencyAppConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class EfficiencyAppConstants
+    {
+        public const decimal MinimumEfficiencyForTopList = 100.0m;
+    }
+}
