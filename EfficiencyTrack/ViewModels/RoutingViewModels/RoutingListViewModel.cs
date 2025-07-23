54edd79078f6d9540d236397234235da3421a8dd
VelizarABB <velizar.stankov@bg.abb.com>
2025-07-23 15:38:31 +0300

    "Improved data visualization, added new UI elements, and optimized for a better user experience."

--- EfficiencyTrack/ViewModels/RoutingViewModels/RoutingListViewModel.cs
+++ EfficiencyTrack/ViewModels/RoutingViewModels/RoutingListViewModel.cs
@@ -3,5 +3,12 @@
     public class RoutingListViewModel
     {
         public List<RoutingViewModel> Routings { get; set; } = [];
+
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
     }
+
 }
